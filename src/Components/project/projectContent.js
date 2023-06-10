dd9759b565303c27052c229c2ade0a3e44f0bf78
rahul kumar <rkrahul8181@gmail.com>
2023-06-10 14:26:37 +0530

    Add project

--- src/Components/project/projectContent.js
+++ src/Components/project/projectContent.js
@@ -4,6 +4,10 @@ import ls from "../../assects/project/lifeStyleStore.gif"
 import vidfy from "../../assects/project/vidfy.gif"
 import cozytrip from "../../assects/project/tripVilla/homePage.png"
 import gearhub_navbar from "../../assects/project/gearhub/Navbar.png"
+import task_manager from "../../assects/project/task-manager/task-manager.png"
+import project_lister from "../../assects/project/project-tracker/dashboard.png"
+import code_sync from "../../assects/project/sync-code/live-editor_editor-page.png"
+import form_lister from "../../assects/project/form-listner/form-lister.png"
 
 import { AiFillHtml5 } from "react-icons/ai"
 import { TbBrandCss3 } from "react-icons/tb"
@@ -79,6 +83,21 @@ export const bookingWeb = {
 
 }
 
+export const taskManagerUsingBdnd = {
+    github: "https://github.com/rahulkr-dev/task_manager-app",
+    live: "https://task-manager-app-wine.vercel.app/",
+    techStackName: [
+        { name: "HTML", color: "green" }, { name: "css", color: "red" },
+        { name: "javascript", color: "purple" }, { name: "reactjs", color: "green" },
+    ],
+    name: "Task Manager",
+    desc: "This is a management app designed to help users organize, prioritize, and track their tasks and projects. One of the key features of such an app is the ability to use drag and drop functionality to easily move tasks between different lists or categories.",
+    techStack: [AiFillHtml5, TbBrandCss3, TbBrandJavascript, FaReact],
+    image: task_manager,
+    type:"react"
+
+};
+
 export const gearHub = {
     github: "https://github.com/rahulkr-dev/GearHub-e-commerce",
     live: "https://frontend-flax-five.vercel.app/",
@@ -95,8 +114,57 @@ export const gearHub = {
 
 };
 
+export const realTimeCodeEditor = {
+    github: "https://github.com/rahulkr-dev/real-time-code-editor",
+    live: "",
+    techStackName: [
+        { name: "HTML", color: "green" }, { name: "css", color: "red" },
+        { name: "javascript", color: "purple" }, { name: "reactjs", color: "green" }, { name: "nodejs", color: "orange" },
+        { name: "expressjs", color: "blue" }, { name: "mongodb", color: "red" },
+    ],
+    name: "CodeSync",
+    desc: "Introducing our Real-Time Group Code Editor, powered by WebSockets. Collaborate and code together, with instant updates for seamless teamwork. Experience coding in sync, as changes happen in real time.",
+    techStack: [AiFillHtml5, TbBrandCss3, TbBrandJavascript, FaReact, SiChakraui, DiNodejs, SiExpress, DiMongodb],
+    image: code_sync,
+    type:"mern"
+
+};
+
+export const formLister = {
+    github: "https://github.com/rahulkr-dev/travelopia_assignment",
+    live: "https://travelopia-frontend.vercel.app/",
+    techStackName: [
+        { name: "HTML", color: "green" }, { name: "css", color: "red" },
+        { name: "javascript", color: "purple" }, { name: "reactjs", color: "green" }, { name: "nodejs", color: "orange" },
+        { name: "expressjs", color: "blue" }, { name: "mongodb", color: "red" },
+    ],
+    name: "Form Lister",
+    desc: "Welcome to our mini project! On the first page, provide your travel details like destination, number of travelers, and budget per person. On the second page, easily view and search the submitted form data for convenient access..",
+    techStack: [AiFillHtml5, TbBrandCss3, TbBrandJavascript, FaReact, SiChakraui, DiNodejs, SiExpress, DiMongodb],
+    image: form_lister,
+    type:"mern"
+
+};
+
+export const projectTracker = {
+    github: "https://github.com/rahulkr-dev/travelopia_assignment",
+    live: "https://travelopia-frontend.vercel.app/",
+    techStackName: [
+        { name: "HTML", color: "green" }, { name: "css", color: "red" },
+        { name: "javascript", color: "purple" }, { name: "reactjs", color: "green" }, { name: "nodejs", color: "orange" },
+        { name: "expressjs", color: "blue" }, { name: "mongodb", color: "red" },
+    ],
+    name: "Project Tracker",
+    desc: "Introducing our web application built on the MERN stack, designed to efficiently manage projects. With the ability to add projects, sort, filter, and paginate, you can easily navigate and view project details in a streamlined manner.",
+    techStack: [AiFillHtml5, TbBrandCss3, TbBrandJavascript, FaReact, SiChakraui, DiNodejs, SiExpress, DiMongodb],
+    image: project_lister,
+    type:"mern"
+
+};
+
+
 const projectArray = [
-    gearHub,zee5,tripvilla,lifeStyle,bookingWeb
+    gearHub,zee5,tripvilla,projectTracker,lifeStyle,bookingWeb,taskManagerUsingBdnd,realTimeCodeEditor,formLister
 ];
 
 
